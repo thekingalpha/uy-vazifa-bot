77d686dcf1f64e4ce711182ec7222a8d2341b548
thekingalpha <usultanov079@gmail.com>
2024-07-16 17:10:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+from googletrans import Translator
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '7398693196:AAH-auQYvhqK7X_FpKsCh7YiT98lUDdYUiM'
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+translator=Translator()
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+
+    await message.reply("Salom!\nBu bot matnni inglizchaga tarjima qiladi!\nKerakli matnni kiriting.")
+
+
+
+@dp.message_handler()
+async def gte_data(message: types.Message):
+    translation=translator.translate(message.text,dest="en")
+    trans_text=translation.text
+    await message.reply(trans_text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
